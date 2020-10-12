c911bf96a2cac8e8b56690d3954397ce1d2852f3
angguns <72248195+enjoene@users.noreply.github.com>
2020-10-12 20:31:00 +0700

    zoom
    ashek

---
+++ js/js/kalkulator.js
@@ -0,0 +1,90 @@
+var bil1;
+var bil2;
+var hasil;
+var opr;
+var opr_seleksi = false;
+function btn(angka) {
+	var display = document.getElementById("output").value;
+	if (display == "0") {
+		display = angka; 
+	} else {
+		display += angka;
+	}
+	document.getElementById("output").value = display;
+}
+ 
+function clr() {
+	document.getElementById("output").value = "0";
+	bil1 = 0;
+	bil2 = 0;
+	opr_seleksi = false;
+}
+ 
+function koma() {
+	var display = document.getElementById("output").value;
+	if (display.includes(".") == false) {
+		display += ".";
+	}
+	document.getElementById("output").value = display;	
+}
+ 
+function btn_opr(o) {
+	opr_seleksi = true;
+	bil1 = parseFloat(document.getElementById("output").value);
+	opr = o;
+	document.getElementById("output").value = "0";
+}
+ 
+function hitung() {
+	if (opr_seleksi == true) {
+		bil2 = parseFloat(document.getElementById("output").value);
+		switch(opr){
+			case 1 :
+				hasil = bil1 * bil2;
+				document.getElementById("output").value = hasil;			
+				break;
+			case 2 :
+				hasil = bil1 / bil2;
+				document.getElementById("output").value = hasil;
+				break;
+			case 3 :
+				hasil = bil1 - bil2;
+				document.getElementById("output").value = hasil;
+				break;
+			case 4 :
+				hasil = bil1 + bil2;
+				document.getElementById("output").value = hasil;
+				break;
+		}
+		opr_seleksi = false
+		hasil = 0;
+		bil1 = 0;
+		bil2 = 0;
+	}
+}
+
+var luas;
+var keliling;
+var lebar;
+var panjang;
+function hitung() {
+	lebar = parseFloat(document.getElementById("ipt_lebar").value);
+	panjang = parseFloat(document.getElementById("ipt_panjang").value);
+	luas = panjang * lebar;
+	keliling = 2 * (panjang + lebar);
+	document.getElementById('opt_luas').value = luas;
+	document.getElementById('opt_keliling').value = keliling;
+}
+const phi = 3.14;
+var luas;
+var keliling;
+var r;
+var d;
+function hitung() {
+	r = parseFloat(document.getElementById("ipt_r").value);
+	luas = phi * Math.pow(r,2);
+	d = 2 * r;
+	keliling = phi * d;
+	document.getElementById('opt_luas').value = luas;
+	document.getElementById('opt_keliling').value = keliling;
+}
